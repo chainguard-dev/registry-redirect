4933513cdf4b5ea2dbf09bb7761f63a8d870d5f7
Jason Hall <imjasonh@gmail.com>
2022-07-27 10:58:17 -0400

    improve e2e test

--- e2e-test.sh
+++ e2e-test.sh
@@ -13,10 +13,17 @@ trap 'kill $PID' EXIT
 
 sleep 3  # Server isn't immediately ready.
 
-curl http://localhost:8080/v2/
-curl -I HEAD http://localhost:8080/v2/nginx/manifests/latest
-curl http://localhost:8080/v2/nginx/manifests/latest
-curl http://localhost:8080/v2/nginx/tags/list
+got=$(curl http://localhost:8080/v2/ --write-out '%{http_code}')
+[[ ! "$got" =~ "401" ]] && { echo "expected 401 from /v2/, got $got"; exit 1; }
+
+got=$(curl -I HEAD http://localhost:8080/v2/nginx/manifests/latest --write-out "%{http_code}")
+[[ ! "$got" =~ "405" ]] && { echo "expected 405 from HEAD /v2/nginx/manifests/latest, got $got"; exit 1; }
+
+got=$(curl -H "Accept: application/vnd.oci.image.index.v1+json" http://localhost:8080/v2/nginx/manifests/latest --write-out "%{http_code}")
+[[ ! "$got" =~ "200" ]] && { echo "expected 200 from /v2/nginx/manifests/latest, got $got"; exit 1; }
+
+got=$(curl http://localhost:8080/v2/nginx/tags/list --write-out "%{http_code}")
+[[ ! "$got" =~ "200" ]] && { echo "expected 200 from /v2/nginx/tags/list, got $got"; exit 1; }
 
 crane digest localhost:8080/nginx
 crane manifest localhost:8080/nginx
